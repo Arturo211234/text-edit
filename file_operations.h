c01fad9169a7c5bd5bb1bd770f12d65aca43396e
aslikr211234 <arturo211234@gmail.com>
2021-01-10 20:30:54 -0600

    fixed memory leak issue

--- file_operations.h
+++ file_operations.h
@@ -29,13 +29,12 @@ size_t openFile(char **buffer, size_t *buffer_size, char *fileName){
  printf("%ld\n", (*buffer_size));
  *buffer = (char *) malloc((*buffer_size) * 2);
  size_t ret_val = fread((*buffer), 1, (*buffer_size), file);
- /*if(ret_val != (*buffer_size)){
+ if(ret_val != (*buffer_size)){
   puts("Unable to read file");
   exit(1);
- }*/
+ } printf("sizeof(buffer): %ld\n", sizeof(buffer));
  printf("Bytes Read: %ld\n", ret_val);
  fclose(file);
- buffer[(*buffer_size)] = '\0'; //!!
  *buffer_size = (*buffer_size) * 2;
  return ret_val;
 }
